e96f9aa2758672e53507c8773fa48249885811c3
SamirArora <94946741+SamirArora@users.noreply.github.com>
2026-08-30 18:39:11 -0700

    Add job script for distributed resampling

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=distributed_resampling
+#SBATCH --output=distributed_resampling_output_%j.log
+#SBATCH --error=distributed_resampling_error_%j.log
+#SBATCH --mail-user=samira@sfu.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --account=def-liang-ab
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=2G
+#SBATCH --time=00:10:00
+#SBATCH --constraint=turin
+#SBATCH --distribution=block:block
+#SBATCH --hint=nomultithread
+#SBATCH --exclusive
+
+# Load Julia only
+module load julia
+
+
+# Run Julia
+numactl --cpunodebind=0,1 --membind=0,1 \
+julia --project=$HOME/scratch/distributed_smc distributed_resample_experiment.jl
